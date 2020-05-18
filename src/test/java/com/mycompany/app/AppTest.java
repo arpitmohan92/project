8b3b2cbb3e7cf49b7fa0c2e62664d6c121b0914e
arpitmohan92 <amsmzn@gmail.com>
2020-05-18 14:38:13 -0400

    commited

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("HHello World!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("HHHello World!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
